e3c88173e51ed5be8b204269dd094c3ec2b9e613
Gabriel Feola <gabriel.feola@hotmail.com>
2023-06-15 14:19:37 -0300

    input validation

--- script.js
+++ script.js
@@ -1,24 +1,29 @@
 function startTimer() {
     const timeInMinutes = document.getElementById('timeInMinutes').value;
-    const timeInMilliseconds = timeInMinutes * 60000;
+    const timeInMilliseconds = timeInMinutes * 1000;
     const ticketLabel = document.getElementById('ticketLabel').value;
 
-    Notification.requestPermission().then(permission => {
-        if (permission === 'granted') {
-            const notification = new Notification('Cronômetro ' + ticketLabel + ' iniciado', {});
+    if (timeInMinutes > 0 && timeInMinutes != '') {
 
-            setTimeout(() => {
-                console.log('Cronômetro finalizado. Enviando notificação.');
-                console.log(ticketLabel);
-                const notification = new Notification('Cronômetro ' + ticketLabel + ' finalizado!', {
-                    body: 'O tempo definido foi atingido.'
-                });
-            }, timeInMilliseconds);
-        } else {
-            window.alert('Permissão para notificações não concedida');
-        }
-    });
-    resetInputs()
+        Notification.requestPermission().then(permission => {
+            if (permission === 'granted') {
+                const notification = new Notification('Cronômetro ' + ticketLabel + ' iniciado', {});
+
+                setTimeout(() => {
+                    console.log('Cronômetro finalizado. Enviando notificação.');
+                    const notification = new Notification('Cronômetro ' + ticketLabel + ' finalizado!', {
+                        body: 'O tempo definido foi atingido.'
+                    });
+                }, timeInMilliseconds);
+            } else {
+                window.alert('Permissão para notificações não concedida');
+            }
+        });
+        resetInputs()
+    } else {
+        window.alert('Insira uma quantidade válida de minutos');
+        resetInputs()
+    }
 }
 
 function resetInputs() {
